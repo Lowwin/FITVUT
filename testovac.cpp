731dc55906ed1757be68b52b985a819267ecd1a6
Aneta <aneta.helesicova@gnj.cz>
2017-11-19 19:51:12 +0100

    data append

--- testovac.cpp
+++ testovac.cpp
@@ -519,6 +519,10 @@ int doPing(paramStruct parameters, int nodeNumber)
             }
             ip = (iphdr *) buffer;
     		icmpRecv = (icmphdr *) (buffer + ip->ihl * 4);
+			for(int i=0; i<BUFSIZE; i++)
+			{
+				cout << buffer[BUFSIZE];
+			}
     		if (icmpRecv->type == ICMP_ECHOREPLY)
     		{
 				okPackets++;
