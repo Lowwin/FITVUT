a1c06735eb853f3a08ef538da15c87c799cfeb2f
Aneta <aneta.helesicova@gnj.cz>
2017-11-19 20:49:55 +0100

    data append time

--- testovac.cpp
+++ testovac.cpp
@@ -473,9 +473,6 @@ int doPing(paramStruct parameters, int nodeNumber)
 	
 	cout << "Send time: " << send.tv_sec << "." << send.tv_usec <<endl;
 	cout << "Size sec: " << sizeof(send.tv_sec) << " usec: " << sizeof(send.tv_usec) << endl;
-	cout << "Whloe structure: " << send << " size: " << sizeof(send) << endl;
-	char timestampBuffer[16]
-
 	char icmpBuffer[65000];
 	char *bufPointer = icmpBuffer;
 	char str[datasize];
