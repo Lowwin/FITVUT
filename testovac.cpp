e55f18989dba877a2a81d6c25afcfe1997521d7e
Aneta <aneta.helesicova@gnj.cz>
2017-11-20 16:28:41 +0100

    ipv6

--- testovac.cpp
+++ testovac.cpp
@@ -830,7 +830,7 @@ int doPing4(paramStruct parameters, int nodeNumber)
 		cout << "ICMP head " << sizeof(icmphdr) << " String " << sizeof(str)-1 << " ip hdr" << sizeof(iphdr) 
 			<< " Timestamp 16" << endl;
 
-		icmp->checksum = checksum((u_short *)icmpBuffer, sizeof(icmphdr)+sizeof(str)-1+sizeof(timestampBuf));
+		icmp->checksum = checksum((u_short *)icmpBuffer, sizeof(icmphdr)+sizeof(str)-1);
 		if(sendto(sock,  (char *)icmpBuffer, sizeof(icmphdr)+sizeof(str)-1, 0, (sockaddr *)&sendSockAddr, sizeof(sockaddr)) <= 0)
 			cout << "DID NOT SEND A THING." << endl;
 		nodes[nodeNumber].hourSent++;
