81b675c0cd0cdfdd924f97df034a717f501ab430
Aneta <aneta.helesicova@gnj.cz>
2017-11-20 09:53:40 +0100

    ipv6

--- testovac.cpp
+++ testovac.cpp
@@ -703,7 +703,8 @@ int doPing6(paramStruct parameters, int nodeNumber)
 		}
 
 		icmp->icmp6_cksum = checksum((u_short *)icmpBuffer, sizeof(icmp6_hdr)+sizeof(str)-1);
-		int errsend = sendto(sock,  (char *)icmpBuffer, sizeof(icmp6_hdr)+sizeof(str)-1, 0, (sockaddr *)&sendSockAddr, sizeof(sockaddr))
+		int errsend = sendto(sock,  (char *)icmpBuffer, sizeof(icmp6_hdr)+sizeof(str)-1, 0,
+							(sockaddr *)&sendSockAddr, sizeof(sockaddr));
 		if(errsend<=0)
 		{	
 			cout << "DID NOT SEND A THING. Error " << errsend << endl;
